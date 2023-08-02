47054ad984942d06191623e506bde259658687d7
Mark Soro <napoleon_soro@rocketmail.com>
2023-08-02 06:02:31 -0400

    updated the script. added comments and fixed the subheading bug

--- script.js
+++ script.js
@@ -59,44 +59,57 @@ let showingSubMenu = false;
 topMenuEl.addEventListener("click", (event) => {
   event.preventDefault();
   let clicked = event.target.textContent;
-  if (clicked === "about") mainEl.innerHTML = `<h1>about</h1>`;
+  if (clicked === "about") mainEl.innerHTML = `<h1>about</h1>`; // I'm adding this here. It makes sense to add here since about doesnt have a subheading link
   if (event.target.tagName === "A") {
     //prettier-ignore
     console.log(`An element from topMenuEl w/ tag name of A was clicked => ${clicked}`);
   }
+
+  /**
+   * -----
+   * NOTE
+   * -----
+   * I didnt really follow the instructions thoroughly. It was confusing but I think I got it work
+   * the way it suppose to work.
+   */
+
   topMenuLinks.forEach((link) => {
-    let isActive;
     if (link.textContent !== clicked) {
       link.classList.remove("active");
       showingSubMenu = false;
       subMenuEl.style.top = "0";
     } else {
-      link.classList.add("active");
+      //in task 5.6, before 5.7, TO ACHIEVE "Clicking an "active" link should clear that link."
+      link.classList.toggle("active"); // I used TOGGLE instead of ADD
     }
   });
 
   menuLinks.forEach((element) => {
-    if (element.text !== "about") {
+    //prettier-ignore
+    if (element.text !== "about" && event.target.getAttribute("class") === "active") {
       showingSubMenu = true;
     } else {
       showingSubMenu = false;
     }
   });
-
-  if (showingSubMenu === true) {
-    buildSubmenu();
+  // since about doensnt have a sub menu, avoid building it on the first place
+  if (showingSubMenu === true && event.target.textContent !== "about") {
+    buildSubmenu(); // I couldn't figure out how to feed the selected subheading array so.. in my buildSubMenu() function...
     subMenuEl.style.top = "100%";
   } else {
     subMenuEl.style.top = "0";
   }
 
+  // I directly iterated the menuLinks object to extract the subheading links.
   function buildSubmenu() {
     subMenuEl.textContent = "";
     menuLinks.forEach((element) => {
-      if (element.text.includes(clicked) && element.text !== "about") {
+      //prettier-ignore
+      if (element.text.includes(clicked)) { // check which object inside the array matches the .text of element clicked
         element.subLinks.forEach((item) => {
           const link = document.createElement("a");
           link.innerText = item.text;
+          link.setAttribute("href", item.href);
           subMenuEl.appendChild(link);
         });
       }
@@ -108,9 +121,7 @@ topMenuEl.addEventListener("click", (event) => {
     console.log(`An element from subHeading w/ nodeName name of ${event.target.nodeName} was clicked => ${event.target.textContent}`);
     showingSubMenu = false;
     subMenuEl.style.top = "0";
-    topMenuLinks.forEach((element) => {
-      element.classList.remove("active");
-    });
+    topMenuLinks.forEach((element) => element.classList.remove("active"));
     mainEl.innerHTML = `<h1>${event.target.textContent}</h1>`;
   });
 });
